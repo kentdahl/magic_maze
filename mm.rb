f0864a2c3e206d9a2d1b3f16a92dd0344a7c3483
menthal <menthal>
2008-07-22 09:43:49 +0000

    Licensing blurb.

--- mm.rb
+++ mm.rb
@@ -1,5 +1,30 @@
 #!/usr/bin/ruby -I.
 
+############################################################
+# This game is FREE as in both BEER and SPEECH. It is available and can 
+# be distributed under the terms of the GPL license (version 2) or 
+# alternatively the dual-licensing terms of Ruby itself.
+# Please see README.txt and COPYING_GPL.txt for details.
+#
+#  Magic Maze - a simple and low-tech monster-bashing maze game.
+#  Copyright (C) 2004-2008 Kent Dahl
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+############################################################
+
 require 'getoptlong'
 
 options = GetoptLong.new(["--help",     "-h", GetoptLong::NO_ARGUMENT ], 
