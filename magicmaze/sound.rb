67b0496b10c243525f665c3c325f380cd074a7b8
menthal <menthal>
2008-10-12 14:22:46 +0000

    Licensing blurbs.

--- magicmaze/sound.rb
+++ magicmaze/sound.rb
@@ -1,3 +1,14 @@
+############################################################
+# Magic Maze - a simple and low-tech monster-bashing maze game.
+# Copyright (C) 2004-2008 Kent Dahl
+#
+# This game is FREE as in both BEER and SPEECH. 
+# It is available and can be distributed under the terms of 
+# the GPL license (version 2) or alternatively 
+# the dual-licensing terms of Ruby itself.
+# Please see README.txt and COPYING_GPL.txt for details.
+############################################################
+
 require 'sdl'
 
 module MagicMaze
